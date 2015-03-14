a67241c43dda86ac57fecd646139c0d0979fc73e
Coen Hacking <coen.hacking@gmail.com>
2015-03-14 16:37:54 +0100

    made it slightly faster

--- src/Board.java
+++ src/Board.java
@@ -73,11 +73,12 @@ public class Board extends JPanel {
 			return null;
 		
 		int de = (l.a.x - l.b.x) * (l2.a.y - l2.b.y) - (l.a.y - l.b.y) * (l2.a.x - l2.b.x);
-		int nu1 = (l2.a.x - l2.b.x) * (l.a.x * l.b.y - l.b.x * l.a.y) - (l.a.x - l.b.x) * (l2.a.x * l2.b.y - l2.b.x * l2.a.y);
-		int nu2 = (l2.a.y - l2.b.y) * (l.a.x * l.b.y - l.b.x * l.a.y) - (l.a.y - l.b.y) * (l2.a.x * l2.b.y - l2.b.x * l2.a.y);
-		
+
 		if (de == 0)
 			return null;
+
+		int nu1 = (l2.a.x - l2.b.x) * (l.a.x * l.b.y - l.b.x * l.a.y) - (l.a.x - l.b.x) * (l2.a.x * l2.b.y - l2.b.x * l2.a.y);
+		int nu2 = (l2.a.y - l2.b.y) * (l.a.x * l.b.y - l.b.x * l.a.y) - (l.a.y - l.b.y) * (l2.a.x * l2.b.y - l2.b.x * l2.a.y);
 		
 		Point p = new Point(0,0);
 		p.x = (int) ((double) nu1 / (double) de);
