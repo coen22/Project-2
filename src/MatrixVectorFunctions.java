3a07a10e375509ec66e13e0087a6c9c7a99bbc12
Coen Hacking <coen.hacking@gmail.com>
2015-03-16 14:44:44 +0100

    updated matrix functions

--- src/MatrixVectorFunctions.java
+++ src/MatrixVectorFunctions.java
@@ -6,9 +6,20 @@
  * @version 1.0
  */
 public class MatrixVectorFunctions {
-	
 	public static boolean intersectExists(LineSegment segA, LineSegment segB){
 		
 		return true;
+	}
+	
+	public static Vertex intersect(LineSegment l, LineSegment l2) {
+		double denominator = (l.getA().getX() - l.getB().getX()) * (l2.getA().getY() - l2.getB().getY()) - (l.getA().getY() - l.getB().getY()) * (l2.getA().getX() - l2.getB().getX());
+
+		if (denominator == 0)
+			return null;
+		
+		double numeratorX = (l2.getA().getX() - l2.getB().getX()) * (l.getA().getX() * l.getB().getY() - l.getB().getX() * l.getA().getY()) - (l.getA().getX() - l.getB().getX()) * (l2.getA().getX() * l2.getB().getY() - l2.getB().getX() * l2.getA().getY());
+		double numeratorY = (l2.getA().getY() - l2.getB().getY()) * (l.getA().getX() * l.getB().getY() - l.getB().getX() * l.getA().getY()) - (l.getA().getY() - l.getB().getY()) * (l2.getA().getX() * l2.getB().getY() - l2.getB().getX() * l2.getA().getY());
+		
+		return new Vertex(numeratorX / denominator, numeratorY / denominator);
 	}
 }
