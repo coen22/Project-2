707ca987c7e9f080f4241159c1d14b617788a8f9
HuskydoT <brianhuynen@planet.nl>
2015-03-16 17:07:21 +0100

    The FileHandler can write, read and delete the contents of a file

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -7,26 +7,42 @@ import java.io.IOException;
 
 public class FileHandler {
 	
-	String fileName = "File.txt";
-	FileWriter writer;
-	BufferedWriter writerB;
-	FileReader reader;
-	BufferedReader readerB;
+	private String fileName;
+	private FileWriter writer;
+	private BufferedWriter writerB;
+	private FileReader reader;
+	private BufferedReader readerB;
+	
+	public FileHandler(String fileName){
+		this.fileName = fileName;
+	}
+	
+	public void deleteFile(){
+		try{
+			writer = new FileWriter(fileName);
+			writerB = new BufferedWriter(writer);
+			writerB.write("");
+			writerB.close();
+		} catch(IOException ex) {
+            System.out.println("Error writing to file '" + fileName + "'");
+        }
+	}
 	
 	public void writeFile(String data){
 		try {
-			writer = new FileWriter(fileName);
+			writer = new FileWriter(fileName, true);
 		    writerB = new BufferedWriter(writer);
             writerB.write(data);
+            writerB.newLine();
             writerB.close();
-        }
-        catch(IOException ex) {
+        } catch(IOException ex) {
             System.out.println("Error writing to file '" + fileName + "'");
         }
 	}
 	
 	public void readFile(){
 		String line = null;
+
         try {
             reader = new FileReader(fileName);
             readerB = new BufferedReader(reader);
@@ -34,11 +50,9 @@ public class FileHandler {
                 System.out.println(line);
             }    
             readerB.close();            
-        }
-        catch(FileNotFoundException ex) {
+        } catch(FileNotFoundException ex) {
             System.out.println("Unable to open file '" + fileName + "'");                
-        }
-        catch(IOException ex) {
+        } catch(IOException ex) {
             System.out.println("Error reading file '" + fileName + "'");                   
         }
 	}
