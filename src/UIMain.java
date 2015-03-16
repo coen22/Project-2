422d09f21dbca24af765404840c7b9bb88e4b537
imrayya <imrayya@gmail.com>
2015-03-16 10:55:30 +0100

    More UI changes

--- src/UIMain.java
+++ src/UIMain.java
@@ -7,7 +7,7 @@ import java.util.Observer;
 import javax.swing.JFrame;
 
 public class UIMain extends JFrame implements Observer {
-
+    private Launch engine;
     public static void main(String[] args) {
         new UIMain();
     }
@@ -18,14 +18,30 @@ public class UIMain extends JFrame implements Observer {
     }
 
     public UIMain() throws HeadlessException {
-
         setFocusable(true);
         setTitle("Polygone Calucations");
         setDefaultCloseOperation(3);
         setSize(800, 600);
+        
+        //creates the actual engine
+        engine = new Launch();
+        //adds the current object as an observer
+        engine.addObserver(this);
+        
+        //Gets the size of the screen
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        //Centers the JFrame to the middle of the screen
         setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+        
+        //Sets the UI to be visiable 
         setVisible(true);
+    }
+
+    /**
+     * Creates the actual user interface
+     */
+    private void inti() {
+
     }
 
 }
