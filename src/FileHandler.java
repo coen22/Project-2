afd77aff2bb2a1aa577ed657f4984a0d20a025ea
HuskydoT <brianhuynen@planet.nl>
2015-03-16 16:49:40 +0100

    Using BufferedReader instead of File

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,38 +1,46 @@
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Formatter;
-import java.util.Scanner;
 
-
-public class FileHandler{
+public class FileHandler {
 	
-	private Scanner scan;
-	private Formatter form;
-	private String data;
-
-	public void openFile(){
-		try{
-			scan = new Scanner(new File("File.txt"));
-			form = new Formatter(new File("File.txt"));
-		} catch(Exception e){
-			System.out.println("File not found - is it in the right directory?");
-		}
-	}
+	String fileName = "File.txt";
+	FileWriter writer;
+	BufferedWriter writerB;
+	FileReader reader;
+	BufferedReader readerB;
 	
 	public void writeFile(String data){
-		form.format("%s%n", data);
+		try {
+			writer = new FileWriter(fileName);
+		    writerB = new BufferedWriter(writer);
+            writerB.write(data);
+            writerB.close();
+        }
+        catch(IOException ex) {
+            System.out.println("Error writing to file '" + fileName + "'");
+        }
 	}
 	
-	public String readFile(){
-		if(scan.hasNextLine()){
-			data = scan.next();
-		}
-		return data;
-	}
-	
-	public void closeFile(){
-		scan.close();
-		form.close();
+	public void readFile(){
+		String line = null;
+
+        try {
+            reader = new FileReader(fileName);
+            readerB = new BufferedReader(reader);
+            while((line = readerB.readLine()) != null) {
+                System.out.println(line);
+            }    
+            readerB.close();            
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                   
+        }
 	}
 }
