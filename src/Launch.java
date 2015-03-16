7535aac758384f651a3cb2995d3e8b5e8c8ee69d
davidr88 <david.roschewitz@gmail.com>
2015-03-16 14:13:46 +0100

    update

--- src/Launch.java
+++ src/Launch.java
@@ -4,18 +4,6 @@ public class Launch extends Observable{
 
 	public static void main(String[] args){
 		
-		LineSegment testsegment1 = new LineSegment(new Vertex(0, 0), new Vertex(100, 100));
-		LineSegment testsegment2 = new LineSegment(new Vertex(100, 100), new Vertex(0, 0));
-		LineSegment testsegment3 = new LineSegment(new Vertex(10, 10), new Vertex(50, 50));
-		
-		LineStack stack1 = new LineStack();
-		
-		stack1.pushBubbleSort(testsegment2);
-		System.out.println(stack1);
-		stack1.pushBubbleSort(testsegment1);
-		System.out.println(stack1);
-		stack1.pushBubbleSort(testsegment3);
-		System.out.println(stack1);
 	}
 
 }
