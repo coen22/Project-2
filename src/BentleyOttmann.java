1b788c815b30fd8a2d15e13a3eddddf13a6efa1a
davidr88 <david.roschewitz@gmail.com>
2015-03-16 16:25:53 +0100

    more

--- src/BentleyOttmann.java
+++ src/BentleyOttmann.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 
 public class BentleyOttmann {
-
+	public static ArrayList<Vertex> findIntersects(PolyLine polyLine){
+		LineStack sweepLineStack = new LineStack();
+		
+		return null;
+	}
 }
