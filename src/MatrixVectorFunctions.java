b42655845d1d1f74605bb9c4cc8a524d9fb167a9
MarcianoG <marcianogeijselaers@hotmail.com>
2015-03-17 22:54:52 +0100

    Added basic methods for intersect use

--- src/MatrixVectorFunctions.java
+++ src/MatrixVectorFunctions.java
@@ -6,6 +6,21 @@
  * @version 1.0
  */
 public class MatrixVectorFunctions {
+	
+	public static boolean onSegment(Vertex p, Vertex q, Vertex r){
+		if(	q.getX() <= Math.max(p.getX(), r.getX()) && q.getX() >= Math.min(p.getX(), r.getX())
+			q.getX() <= Math.max(p.getY(), r.getY()) && q.getY() >= Math.min(p.getY(), r.getY()))
+		
+		return false;
+	}
+	public static int orientation(Vertex p, Vertex q, Vertex r){
+		int value = (int) ((q.getY() - p.getY())*(r.getX() - q.getX()) -
+					(q.getX() - p.getX())*(r.getY() - q.getY()));
+		if(value == 0){
+			return 0;
+		}
+		return value;
+	}
 	public static boolean intersectExists(LineSegment l, LineSegment l2){
 		double denominator = (l.getA().getX() - l.getB().getX()) * (l2.getA().getY() - l2.getB().getY()) - (l.getA().getY() - l.getB().getY()) * (l2.getA().getX() - l2.getB().getX());
 
