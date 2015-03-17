624b5c89808cf740634872c444748f769d06b0f1
davidr88 <david.roschewitz@gmail.com>
2015-03-17 10:24:47 +0100

    changes

--- src/EventList.java
+++ src/EventList.java
@@ -4,30 +4,46 @@ public class EventList<EventPoint> implements DoublyLinkedListADT<EventPoint>{
 	 private Node<EventPoint> trailer;
 	 private int size;
 	 
+	 public EventList() {
+		 header = new Node<EventPoint>();
+		 trailer = new Node<EventPoint>();
+	     header.setAfter(trailer);
+	     trailer.setBefore(header);
+	     size = 0;
+	}
+	 
 	 @Override
 	 public int size(){
 		 return size;
 	 }
 	    
 	public EventPoint deQueue(){
-		if (size() != 0){
-			return header.getAfter().getElement();
+		if (isEmpty()){
+			return null;
 		}
-		return null;
+		else{
+			Node<EventPoint> returnNode = header.getAfter();
+			EventPoint returnElement = returnNode.getElement();
+			header.setAfter(returnNode.getAfter());
+			returnNode.getAfter().setBefore(header);
+			returnNode = null;
+			size--;
+			return returnElement;
+
+		}
 	}
 	
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (size==0);
 	}
 
 	@Override
 	public EventPoint elementAt(int r) throws EmptySequenceException {
-		if (size == 0){
+		if (isEmpty()){
 			throw new EmptySequenceException();
 		}
-		else if (r < size/2){
+		else if (r <= size/2){
 			Node<EventPoint> current = header.getAfter();
 			for (int i = 0; i < r; i++){
 				current = current.getAfter();
@@ -36,7 +52,7 @@ public class EventList<EventPoint> implements DoublyLinkedListADT<EventPoint>{
 		}
 		else{
 			Node<EventPoint> current = trailer.getBefore();
-			for (int i = size; i > r; i--){
+			for (int i = size-1; i > r; i--){
 				current = current.getBefore();
 			}
 			return current.getElement();
@@ -45,8 +61,12 @@ public class EventList<EventPoint> implements DoublyLinkedListADT<EventPoint>{
 
 	@Override
 	public void insertLast(EventPoint e) {
-		// TODO Auto-generated method stub
-		
+		Node<EventPoint> x = new Node<EventPoint>(e);
+        x.setAfter(trailer);
+        x.setBefore(trailer.getBefore());
+        trailer.getBefore().setAfter(x);
+        trailer.setBefore(x);
+        size++;
 	}
 
 	public Node<EventPoint> getTrailer() {
@@ -56,5 +76,15 @@ public class EventList<EventPoint> implements DoublyLinkedListADT<EventPoint>{
 	public Node<EventPoint> getHeader() {
 		return header;
 	}
+	
+	public String toString() {
+        String string = "";
+        Node<EventPoint> currentNode = header.getAfter();
+        for (int i = 0; i < size; i++) {
+            string = string + "[" + currentNode.getElement() + "]";
+            currentNode = currentNode.getAfter();
+        }
+        return string;
+    }
 
 }
