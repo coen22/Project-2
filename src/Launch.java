7370c651ed380251614e1c60e869e93edc0da5de
imrayya <imrayya@gmail.com>
2015-03-17 10:23:54 +0100

    MOVING STUFF\

--- src/Launch.java
+++ src/Launch.java
@@ -7,20 +7,20 @@ public class Launch extends Observable {
     private ArrayList<PolyLine> listOfPolyLine = new ArrayList();
 
     public Launch() {
-        PolyLine a = new PolyLine(new Vertex(0, 0));
-        Vertex b = new Vertex(100, 0);
-        Vertex c = new Vertex(100, 100);
-        Vertex d = new Vertex(0, 100);
-        Vertex e = new Vertex(594, 38);
-        Vertex f = new Vertex(433, 394);
-        
+        PolyLine a = new PolyLine(new Vertex(300, 300));
+        Vertex b = new Vertex(300, 600);
+        Vertex c = new Vertex(600, 600);
+        Vertex d = new Vertex(573, 462);
+
+        Vertex e = new Vertex(23, 98);
+
         a.insertLast(b);
         a.insertLast(c);
         a.insertLast(d);
         a.insertLast(e);
-        a.insertLast(f);
-        
-        
+        a.closeLine();
+        System.out.println(a);
+
         listOfPolyLine.add(a);
     }
 
@@ -32,24 +32,21 @@ public class Launch extends Observable {
         this.listOfPolyLine = listOfPolyLine;
     }
 
-    
-    
     public static void main(String[] args) {
 
         PolyLine a = new PolyLine(new Vertex(0, 0));
         Vertex b = new Vertex(10, 0);
         Vertex c = new Vertex(10, 10);
         Vertex d = new Vertex(0, 10);
+        Vertex e = new Vertex(0, 0);
         a.insertLast(b);
         a.insertLast(c);
         a.insertLast(d);
-        System.out.println(a.isClosed());
-        a.closeLine();
-        System.out.println(a.isClosed());
+        a.insertLast(e);
 //		System.out.println(a.length());
 //      System.out.println(a.area());
         System.out.println(a);
-        
+
         LineSegmentList<LineSegment> segList = new LineSegmentList<LineSegment>(a);
         System.out.println(segList);
     }
