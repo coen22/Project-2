a567118f38bd5f712bb82408a5edee7fdaf459d9
davidr88 <david.roschewitz@gmail.com>
2015-03-17 12:45:09 +0100

    new

---
+++ src/AVLTreeDummy.java
@@ -0,0 +1,15 @@
+
+public class AVLTreeDummy {
+
+	public boolean exists(EventPoint point){
+		return false;
+	}
+	
+	public void deleteFirst(){
+		
+	}
+	
+	public void sortedInsert(EventPoint newPoint){
+		
+	}
+}
