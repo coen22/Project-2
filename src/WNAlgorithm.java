5d2bb09c5fa577f303f79aead849a93370664878
HuskydoT <brianhuynen@planet.nl>
2015-03-18 13:34:46 +0100

    Eh, close 'nuff (DONE(?))

--- src/WNAlgorithm.java
+++ src/WNAlgorithm.java
@@ -2,40 +2,55 @@ public class WNAlgorithm {
 	private PolyLine line;
 	private Vertex point;
 	private LineSegment segment;
-	private Node<Vertex> header, trailer, current;
+	private Node<Vertex> header, trailer;
 	private int wn;
 	private boolean pointCrossesEdge;
 	
 	public WNAlgorithm(PolyLine line, Vertex point){
 		this.line = line;
 		this.point = point;
-	}
-	
-	public boolean algoritm(){
-		return isOutside();
-	}
-	
+	}		
 	private boolean isOutside(){
+		boolean done = false;
+		Node<Vertex> current = header.getAfter();
+		while (!done){
+			if(movesDownward(current)){
+				if(!isLeft(current, point)){
+					wn--;
+				}
+			}
+			if(movesUpward(current)){
+				if(isLeft(current, point)){
+					wn++;
+				}
+			}
+			if(current.getAfter() == trailer){
+				done = true;
+			}
+		}
 		return wn != 0;
-	}
-	
-	private boolean movesUpward(){
+	}	
+	public boolean isLeft(Node<Vertex> current, Vertex point){
+		double calc = ( (current.getAfter().getElement().getX() - current.getElement().getX())
+							*(point.getY() - current.getElement().getY()) - (point.getX() - current.getElement().getX())
+								*(current.getAfter().getElement().getY() - current.getElement().getY()) );
+		return calc > 0;
+	}	
+	private boolean movesUpward(Node<Vertex> current){
 		return current.getElement().getY() < current.getAfter().getElement().getY();
-	}
-	
-	private boolean movesDownward(){
+	}	
+	private boolean movesDownward(Node<Vertex> current){
 		return current.getElement().getY() > current.getAfter().getElement().getY();
-	}
-	
-	private boolean pointCrossesEdge(){
+	}	
+	private boolean pointCrossesEdge(Node<Vertex> current){
 		pointCrossesEdge = false;
-		if(movesUpward()){
-			if((point.getX() > current.getElement().getX())&&(point.getX() < current.getAfter().getElement().getX())){
+		if(movesUpward(current)){
+			if((point.getY() > current.getElement().getY())&&(point.getY() < current.getAfter().getElement().getY())){
 				pointCrossesEdge = true;
 			}
 		}
-		if(movesDownward()){
-			if((point.getX() < current.getElement().getX())&&(point.getX() > current.getAfter().getElement().getX())){
+		if(movesDownward(current)){
+			if((point.getY() < current.getElement().getY())&&(point.getY() > current.getAfter().getElement().getY())){
 				pointCrossesEdge = true;
 			}
 		}
