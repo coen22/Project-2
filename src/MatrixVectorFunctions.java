d321965992f71fb0179acde3ee7cb658a30b1553
MarcianoG <marcianogeijselaers@hotmail.com>
2015-03-18 11:09:44 +0100

    Added javadoc and the doesintersect now accepts lineSegments

--- src/MatrixVectorFunctions.java
+++ src/MatrixVectorFunctions.java
@@ -7,23 +7,33 @@
  */
 public class MatrixVectorFunctions {
 	
-	public static boolean doesIntersect(Vertex p1, Vertex p2, Vertex q1, Vertex q2){
+	/**
+	 * Checks an Intersect through the use of orientation between the endpoints of the lines
+	 * @param 	line1 Line segment 1
+	 * @param 	line2 Line segment 2
+	 * @return 	Boolean return. Returns true when an intersect is found on the linesegments
+	 * 			Returns false when there is no intersect on the line segments
+	 */
+	public static boolean doesIntersect(LineSegment line1, LineSegment line2){
+		Vertex p1 = line1.getA();
+		Vertex p2 = line1.getB();
+		Vertex q1 = line2.getA();
+		Vertex q2 = line2.getA();
+		
 		int o1 = orientation(p1, p2, q1);
 		int o2 = orientation(p1, p2, q2);
 		int o3 = orientation(q1, q2, p1);
 		int o4 = orientation(q1, q2, p2);
 		
-		System.out.println("o1 = " + o1 + " o2 = " + o2 + " o3 = " + o3 + " o4 = " + o4);
+		
 		//General case
 		if(o1 != o2 && o3 != o4){
-			System.out.println("Enters General case");
 			return true;
 		}
 		//Special cases
 		//ie one line extending another, two segments 'in' eachother
 		
 		if(o1 == 0 && onSegment(p1, q1, p2)){
-			System.out.println("Special Case 1 ");
 			return true;
 		}
 		if(o2 == 0 && onSegment(p1, q2, p2)){
@@ -37,6 +47,13 @@ public class MatrixVectorFunctions {
 		}
 		return false;
 	}
+	/**
+	 * Checks if two segments are overlapping only needed in special cases
+	 * @param p Points to be checked
+	 * @param q
+	 * @param r
+	 * @return 	a boolean which states true if it does lay on the segment
+	 */
 	public static boolean onSegment(Vertex p, Vertex q, Vertex r){
 		if(	q.getX() <= Math.max(p.getX(), r.getX()) && q.getX() >= Math.min(p.getX(), r.getX()) &&
 			q.getY() <= Math.max(p.getY(), r.getY()) && q.getY() >= Math.min(p.getY(), r.getY())){
@@ -44,14 +61,28 @@ public class MatrixVectorFunctions {
 		}
 		return false;
 	}
+	/**
+	 * Checks if the orientation is clockwise, counterclockwise or colinear
+	 * @param p Point 1
+	 * @param q Point 2
+	 * @param r Point 3
+	 * @return The value which can either be -1, 0, 1
+	 */
 	public static int orientation(Vertex p, Vertex q, Vertex r){
 		int value = (int) ((q.getY() - p.getY())*(r.getX() - q.getX()) -
 					(q.getX() - p.getX())*(r.getY() - q.getY()));
 		if(value == 0){
 			return 0;
 		}
-		return value;
+		else if(value > 0){
+			return 1;
+		}
+		else{
+			return -1;
+		}
 	}
+	
+	//Not working and Unused code
 	public static boolean intersectExists(LineSegment l, LineSegment l2){
 		double denominator = (l.getA().getX() - l.getB().getX()) * (l2.getA().getY() - l2.getB().getY()) - (l.getA().getY() - l.getB().getY()) * (l2.getA().getX() - l2.getB().getX());
 
