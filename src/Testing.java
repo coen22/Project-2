97e9e1c8d828b577eedd841dcc369fc87c56940f
davidr88 <david.roschewitz@gmail.com>
2015-03-19 14:24:18 +0100

    edit

--- src/Testing.java
+++ src/Testing.java
@@ -7,37 +7,30 @@ public class Testing {
 		polyLine.insertLast(new Vertex(6, 0));
 		polyLine.insertLast(new Vertex(3, 3));
 		polyLine.closeLine();
-		
-		PolyLine polyLine2 = new PolyLine();
-		polyLine2.insertLast(new Vertex(10, 10));
-		polyLine2.insertLast(new Vertex(16, 10));
-		polyLine2.insertLast(new Vertex(13, 13));
-		polyLine2.closeLine();
-		
-		System.out.println(polyLine +"   " + polyLine2);
-		
-		LineSegmentList list2 = new LineSegmentList(polyLine, polyLine2);
-		
-		System.out.println(list2);
-		
-		BentleyOttmann.polyLinesIntersecting(polyLine, polyLine2);
-//		BentleyOttmann.findIntersects(polyLine, null);
-		
+
 		LineSegmentList list = new LineSegmentList();
 		
-		LineSegment lineseg1 = new LineSegment(new Vertex(3,0), new Vertex(3,5));
-		LineSegment lineseg2 = new LineSegment(new Vertex(3,5), new Vertex(3,10));
-		LineSegment lineseg3 = new LineSegment(new Vertex(2,-1), new Vertex(1,-2));
-		LineSegment lineseg4 = new LineSegment(new Vertex(1,-2), new Vertex(-1,2));
-		LineSegment lineseg5 = new LineSegment(new Vertex(-1,-1), new Vertex(4,1));
+		LineSegment lineseg1 = new LineSegment(new Vertex(-2,-1), new Vertex(2,1));
+		LineSegment lineseg2 = new LineSegment(new Vertex(-1,-2), new Vertex(1,2));
+		LineSegment lineseg3 = new LineSegment(new Vertex(1,9), new Vertex(4,9));
+		LineSegment lineseg4 = new LineSegment(new Vertex(2,10), new Vertex(6,7));
+		LineSegment lineseg5 = new LineSegment(new Vertex(4,8), new Vertex(7,8));
+		LineSegment lineseg6 = new LineSegment(new Vertex(5,10), new Vertex(9,6));
+		LineSegment lineseg7 = new LineSegment(new Vertex(6,5), new Vertex(10,9));
 		
 		list.insertLast(lineseg1);
 		list.insertLast(lineseg2);
 //		list.insertLast(lineseg3);
+
 //		list.insertLast(lineseg4);
 //		list.insertLast(lineseg5);
+//		list.insertLast(lineseg6);
+//		list.insertLast(lineseg7);
 		
-//		BentleyOttmann.findIntersects(null, list);
+		
+//		System.out.println(list);
+		
+		BentleyOttmann.findIntersects(null, list);
 //		BentleyOttmann.findIntersects(polyLine, null);
 		
 	}
