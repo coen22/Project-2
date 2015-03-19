88c09617aae616a9b9d32fcde7e3eacf5d09e58b
davidr88 <david.roschewitz@gmail.com>
2015-03-19 23:51:59 +0100

    final edits sweepLine

--- src/SweepLineRB.java
+++ src/SweepLineRB.java
@@ -3,14 +3,17 @@ import java.util.TreeMap;
 
 public class SweepLineRB {
 	
-	private final boolean DEBUG = false;
+	private final boolean DEBUG = true;
 	private TreeMap<LineSegment, LineSegment> treeList;
 
 	public SweepLineRB(){
 		treeList = new TreeMap<LineSegment, LineSegment>();
 	}
 	
-	public void swap(LineSegment intersectA, LineSegment intersectB, double x) {
+	public LineSegment[] swap(LineSegment intersectA, LineSegment intersectB, double x) {
+		if (DEBUG) System.out.println("swapping line " + intersectA + " with line " + intersectB);
+		
+		//array that provides information to the algorithm which new line-segments meet or must be checked for intersects. Index 0 is the higher segment, 1 the new lower element.
 		LineSegment[] returnArray = {null,null};
 		
 		treeList.remove(intersectA);
@@ -18,54 +21,17 @@ public class SweepLineRB {
 		intersectB.setComparisonXValue(x);
 		treeList.put(intersectA, intersectA);
 		
-		System.out.println("\n-------------------------------------");
-		System.out.println(treeList);
-		System.out.println("intersectA: " + intersectA + ". intersectB: " + intersectB);
-		System.out.println("higherKey: " + treeList.higherKey(intersectB));
-		System.out.println("lowerKey: " + treeList.lowerKey(intersectA));
-		System.out.println(treeList);
-		System.out.println("-------------------------------------\n");
+		returnArray[0] = treeList.higherKey(intersectB);
+		returnArray[1] = treeList.lowerKey(intersectA);
 		
-//		while (counter < list.size() && intersectA.equals(list.get(counter)) != true){
-//			counter++;
-//		}
-//		if (DEBUG) System.out.println("counter: " + counter);
-//		if (counter >= list.size()){
-//			return returnArray;
-//		}
-//		if (counter + 1 < list.size() && list.get(counter+1) == intersectB){ // A is above B
-//			if (DEBUG) System.out.println("intersect B is below A"); 
-//			list.set(counter, intersectB);
-//			list.set(counter+1, intersectA);
-//			if (counter-1>= 0){
-//				returnArray[0] = list.get(counter-1);
-//				if (DEBUG) System.out.println("returning new above: " + returnArray[0]);
-//			}
-//			if (counter + 2 < list.size()){
-//				returnArray[1] = list.get(counter+2);
-//				if (DEBUG) System.out.println("returning new below: " + returnArray[1]);
-//			}
-//		}
-//		else if (counter - 1 >= 0 && list.get(counter-1) == intersectB){ // B is above A, this case should not happen
-//			if (DEBUG) System.out.println("intersect B is above A");
-//			list.set(counter, intersectB);
-//			list.set(counter-1, intersectA);
-//			if (counter-2>= 0){
-//				returnArray[0] = list.get(counter-2);
-//				if (DEBUG) System.out.println("returning new above: " + returnArray[0]);
-//			}
-//			if (counter + 1 < list.size()){
-//				returnArray[1] = list.get(counter+1);
-//				if (DEBUG) System.out.println("returning new below: " + returnArray[1]);
-//			}
-//		}		
-//		return returnArray;
-		
-		System.out.println(treeList);
+		if (DEBUG) System.out.println(treeList);
+		return returnArray;
 	}
 	
 	public LineSegment[] delete(LineSegment lineSegment){
 		if (DEBUG) System.out.println("\nDeleting: " + lineSegment);
+		
+		//array that provides information to the algorithm which new line-segments meet or must be checked for intersects. Index 0 is the higher segment, 1 the new lower element.
 		LineSegment[] returnArray = {null,null};
 
 		returnArray[0] = treeList.higherKey(lineSegment);
@@ -73,7 +39,7 @@ public class SweepLineRB {
 		
 		treeList.remove(lineSegment);
 		
-		System.out.println(treeList);
+		if (DEBUG) System.out.println(treeList);
 		return returnArray;
 	}
 	
@@ -92,8 +58,7 @@ public class SweepLineRB {
 		returnArray[0] = treeList.higherKey(lineSegment);
 		returnArray[1] = treeList.lowerKey(lineSegment);
 		
-		System.out.println(treeList);
-		
+		if (DEBUG)System.out.println(treeList);
 		return returnArray;
 	}
 
--- src/Testing.java
+++ src/Testing.java
@@ -29,38 +29,38 @@ public class Testing {
 		
 //		System.out.println(list);
 		
-		BentleyOttmann.findIntersects(null, list);
+//		BentleyOttmann.findIntersects(null, list);
 //		BentleyOttmann.findIntersects(polyLine, null);
 		
-//		//----------------------------------------------------------------------------------
-//		//below here for Tree Testing
-//		
-//		//new tree
-//		SweepLineRB RB = new SweepLineRB();
-//		
-//		//inserted into the SL
-//		RB.insertSorted(lineseg1);
-//		
-//		
-//		RB.insertSorted(lineseg4);
-//		RB.insertSorted(lineseg2);
-//		RB.insertSorted(lineseg3);
-//		
-//		
-//		//these lines are swaps which are simulated to be in an event queue, this corresponds to the picture in the FB chat
-//		RB.swap(lineseg4, lineseg1, 6.5);
-//		RB.swap(lineseg2, lineseg3, 7);
-//		
-//		RB.insertSorted(lineseg5);
-//		
-//		RB.delete(lineseg1);
-//		
-//		RB.swap(lineseg5, lineseg2, 8);
-//		
-//		RB.delete(lineseg3);
-//		RB.delete(lineseg2);
-//		RB.delete(lineseg4);
-//		RB.delete(lineseg5);
+		//----------------------------------------------------------------------------------
+		//below here for Tree Testing
+		
+		//new tree
+		SweepLineRB RB = new SweepLineRB();
+		
+		//inserted into the SL
+		RB.insertSorted(lineseg1);
+		
+		
+		RB.insertSorted(lineseg4);
+		RB.insertSorted(lineseg2);
+		RB.insertSorted(lineseg3);
+		
+		
+		//these lines are swaps which are simulated to be in an event queue, this corresponds to the picture in the FB chat
+		RB.swap(lineseg4, lineseg1, 6.5);
+		RB.swap(lineseg2, lineseg3, 7);
+		
+		RB.insertSorted(lineseg5);
+		
+		RB.delete(lineseg1);
+		
+		RB.swap(lineseg5, lineseg2, 8);
+		
+		RB.delete(lineseg3);
+		RB.delete(lineseg2);
+		RB.delete(lineseg4);
+		RB.delete(lineseg5);
 		
 	}
 
